cf83ec6d2ad29245d8a58db730648e2b82a494bf
Meanderthal00 <meanderthal00@yahoo.com>
2017-12-02 09:06:16 -0500

    Button rendering corrected

--- assets/JavaScript/giftastic.js
+++ assets/JavaScript/giftastic.js
@@ -7,48 +7,47 @@ $(function () {
 
 // create topic variables
 
-var topicArray = [ 
-    "Leonardo Da Vinci",
-    "Gustav Klimt",
-    "Vincent van Gogh",
-    "Henri Matisse",
-    "Claude Monet",
-    "Vermeer",
-    "Frida Kahlo",
-    "Rene Magritte",
-    "Salvador Dalí",
-    "Pablo Picasso",
- ];
-  // need function to clear exsisting boxes on reload
-  $("#buttonArea").empty();
-
+var topicArray = ["Leonardo Da Vinci", "Gustav Klimt", "Vincent van Gogh", "Henri Matisse", "Claude Monet", "Vermeer", "Frida Kahlo" ,"Rene Magritte", "Salvador Dalí", "Pablo Picasso"];
+ 
 // page loads with 10 exsisting buttons and search box for user input
-    // use a loop that appends a button for each string in the array
-    function createButtons(){
+    
+    function createButtons(){   
+
+        //  function to clear exsisting boxes on reload    
+        $("#buttonArea").empty();
+
+        // use a loop that appends a button for each string in the array
 
         for ( var i = 0; i < topicArray.length; i++ ) {
             var buttons = $("<button>" + topicArray[i] + "</button>")
-            buttons.appendTo("#buttonArea");
+
+            // appending the created buttons to the html
+            buttons.prependTo("#buttonArea");
                 
             };
-        }        
-    
+    }        createButtons();
+
+  
+
 // buttons link to giphy to pull 10 gifs for the selected topic
     // gifs are displayed in a grid in the appropriate div (#"displayArea")
         // initally the gif should be static
         // upon clicking the gif should animate
      // another click stops the animation
+
+
 // Below each gif must also display the following:
     // its rating provided by the GIPHY API
      
 //COMPLETE THE ABOVE STEPS BEFORE CONTIUING TO THE NEXT STEPS 
 
 //   create a form that takes the value from user input and adds it to the "gifTopic" array
+
         $(".form-group").on("click", function(event) {
-            var artist = $(   ).val.trim();
+            var artist = $("#artInput").val.trim();
             topicArray.push(artist);
 
-            createButtons();
+            
         });
         
 // create a function that takes each topic in the array and remakes the buttons on the page prepending the user values to the list
