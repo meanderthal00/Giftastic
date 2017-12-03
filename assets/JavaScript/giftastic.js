9d06e790651168de6455ca5993a5f2bfb7ffbe04
Meanderthal00 <meanderthal00@yahoo.com>
2017-12-02 20:18:07 -0500

    got the dynamic button addition to work correctlygit add -A!

--- assets/JavaScript/giftastic.js
+++ assets/JavaScript/giftastic.js
@@ -7,57 +7,82 @@ $(function () {
 
 // create topic variables
 
-var topicArray = ["Leonardo Da Vinci", "Gustav Klimt", "Vincent van Gogh", "Henri Matisse", "Claude Monet", "Vermeer", "Frida Kahlo" ,"Rene Magritte", "Salvador Dalí", "Pablo Picasso"];
- 
+var topicArray = ["Leonardo Da Vinci", "Gustav Klimt", "Vincent van Gogh", "Henri Matisse", "Claude Monet", "Vermeer", "Frida Kahlo", "Rene Magritte", "Salvador Dalí", "Pablo Picasso"]
+//  ,
+//  selected: " ",
+//  userInput:" ",
+//  giphyApi:,
+//  giphyKey:"wZtZQpZDcla05c5wcbCNYPnygRQoLBYb",
+
+
 // page loads with 10 exsisting buttons and search box for user input
-    
-    function createButtons() {   
 
-        //  function to clear exsisting boxes on reload    
-        $("#buttonArea").empty();
+function createButtons() {
 
-        // use a loop that appends a button for each string in the array
+    //  function to clear exsisting boxes on reload    
+    $("#buttonArea").empty();
 
-        for ( var i = 0; i < topicArray.length; i++ ) {
-            var buttons = $("<button>" + topicArray[i] + "</button>")
-            buttons.addClass("button");
+    // use a loop that appends a button for each string in the array
 
-            // appending the created buttons to the html
-            buttons.prependTo("#buttonArea");
-                
-            }
+    for (var i = 0; i < topicArray.length; i++) {
 
-            //  // adding button functionality
-             $(".button").on("click", function() {
-                 console.log( topicArray[i] + "button logged");
-             })
-    }; 
-      createButtons();
-    
+        var buttons = $("<button>");
+        //  + topicArray[i] + "</button>");
 
-    
+        // adding class
+        buttons.addClass("artist");
 
-  
+        // adding Data attribute
+        buttons.addClass("data-name", topicArray[i]);
+        // adding text with value of the i var
+        buttons.text(topicArray[i]);
+        // adding button to html
+        $("#buttonArea").append(buttons);
+    }
+}    
+
+    //  // adding button functionality
+    $("#searchButton").on("click", function (event) {
+        
+        // preventing the form submit
+        event.preventDefault();
+
+        // grabbing the text from the #artInput text box
+        var userArtist = $("#artInput").val().trim();
+        // pushing user input to the array
+        topicArray.push(userArtist);
+
+        createButtons();
+
+    });
+
+
+
+
+
+
+
 
 // buttons link to giphy to pull 10 gifs for the selected topic
-    // gifs are displayed in a grid in the appropriate div (#"displayArea")
-        // initally the gif should be static
-        // upon clicking the gif should animate
-     // another click stops the animation
+// gifs are displayed in a grid in the appropriate div (#"displayArea")
+// initally the gif should be static
+// upon clicking the gif should animate
+// another click stops the animation
 
 
 // Below each gif must also display the following:
-    // its rating provided by the GIPHY API
-     
+// its rating provided by the GIPHY API
+
 //COMPLETE THE ABOVE STEPS BEFORE CONTIUING TO THE NEXT STEPS 
 
 //   create a form that takes the value from user input and adds it to the "gifTopic" array
 
-        $(".form-group").on("click", function(event) {
-            var artist = $("#artInput").val.trim();
-            topicArray.push(artist);
+// $(".form-group").on("click", function(event) {
+//     var artist = $("#artInput").val.trim();
+//     topicArray.push(artist);
 
-            
-        });
-        
-// create a function that takes each topic in the array and remakes the buttons on the page prepending the user values to the list
+
+// });
+
+// create a function that takes each topic in the array and remakes the     buttons on the page prepending the user values to the list
+createButtons();
