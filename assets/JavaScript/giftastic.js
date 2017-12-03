1297ff52a373e12b294ac64ef3a8268213c3b128
Meanderthal00 <meanderthal00@yahoo.com>
2017-12-02 20:45:15 -0500

    trying to use objects and string literals

--- assets/JavaScript/giftastic.js
+++ assets/JavaScript/giftastic.js
@@ -6,83 +6,91 @@ $(function () {
 });
 
 // create topic variables
-
 var topicArray = ["Leonardo Da Vinci", "Gustav Klimt", "Vincent van Gogh", "Henri Matisse", "Claude Monet", "Vermeer", "Frida Kahlo", "Rene Magritte", "Salvador Dalí", "Pablo Picasso"]
-//  ,
-//  selected: " ",
-//  userInput:" ",
-//  giphyApi:,
-//  giphyKey:"wZtZQpZDcla05c5wcbCNYPnygRQoLBYb",
+
+// attempt at doing it as an object below
+// var topicObject = {
+//         topicArray: ["Leonardo Da Vinci", "Gustav Klimt", "Vincent van Gogh", "Henri Matisse", "Claude Monet", "Vermeer", "Frida Kahlo", "Rene Magritte", "Salvador Dalí", "Pablo Picasso"],
+//         selected: " ",
+//         userInput: " ",
+//         giphyApi: "https://api.giphy.com/v1/gifs/search?",
+//         giphyKey: "wZtZQpZDcla05c5wcbCNYPnygRQoLBYb",
+// }
+// // trying string literal to log the attributes of the  object
+// console.log(`the array is: ${topicObject.gihyApi}`);
+        // page loads with 10 exsisting buttons and search box for user input
+
+        function createButtons() {
+
+            //  function to clear exsisting boxes on reload    
+            $("#buttonArea").empty();
+
+            // use a loop that appends a button for each string in the array
+
+            for (var i = 0; i < topicArray.length; i++) {
+
+                var buttons = $("<button>");
+                //  + topicArray[i] + "</button>");
+
+                // adding class
+                buttons.addClass("artist");
+
+                // adding Data attribute
+                buttons.addClass("data-name", topicArray[i]);
+                // adding text with value of the i var
+                buttons.text(topicArray[i]);
+                // adding button to html
+                $("#buttonArea").append(buttons);
+            }
+        }
+
+        //  // adding button functionality
+
+        $("#searchButton").on("click", function (event) {
+
+            // preventing the form submit
+            event.preventDefault();
+
+            // grabbing the text from the #artInput text box
+            var userArtist = $("#artInput").val().trim();
+            // pushing user input to the array
+            topicArray.push(userArtist);
+
+            createButtons();
+
+            // TODO: find out how to clear the input after submitting
+            $("#artInput").empty();
 
 
-// page loads with 10 exsisting buttons and search box for user input
+        });
 
-function createButtons() {
 
-    //  function to clear exsisting boxes on reload    
-    $("#buttonArea").empty();
 
-    // use a loop that appends a button for each string in the array
 
-    for (var i = 0; i < topicArray.length; i++) {
 
-        var buttons = $("<button>");
-        //  + topicArray[i] + "</button>");
 
-        // adding class
-        buttons.addClass("artist");
 
-        // adding Data attribute
-        buttons.addClass("data-name", topicArray[i]);
-        // adding text with value of the i var
-        buttons.text(topicArray[i]);
-        // adding button to html
-        $("#buttonArea").append(buttons);
-    }
-}    
 
-    //  // adding button functionality
-    $("#searchButton").on("click", function (event) {
-        
-        // preventing the form submit
-        event.preventDefault();
+        // buttons link to giphy to pull 10 gifs for the selected topic
+        // gifs are displayed in a grid in the appropriate div (#"displayArea")
+        // initally the gif should be static
+        // upon clicking the gif should animate
+        // another click stops the animation
 
-        // grabbing the text from the #artInput text box
-        var userArtist = $("#artInput").val().trim();
-        // pushing user input to the array
-        topicArray.push(userArtist);
 
+        // Below each gif must also display the following:
+        // its rating provided by the GIPHY API
+
+        //COMPLETE THE ABOVE STEPS BEFORE CONTIUING TO THE NEXT STEPS 
+
+        //   create a form that takes the value from user input and adds it to the "gifTopic" array
+
+        // $(".form-group").on("click", function(event) {
+        //     var artist = $("#artInput").val.trim();
+        //     topicArray.push(artist);
+
+
+        // });
+
+        // create a function that takes each topic in the array and remakes the     buttons on the page prepending the user values to the list
         createButtons();
-
-    });
-
-
-
-
-
-
-
-
-// buttons link to giphy to pull 10 gifs for the selected topic
-// gifs are displayed in a grid in the appropriate div (#"displayArea")
-// initally the gif should be static
-// upon clicking the gif should animate
-// another click stops the animation
-
-
-// Below each gif must also display the following:
-// its rating provided by the GIPHY API
-
-//COMPLETE THE ABOVE STEPS BEFORE CONTIUING TO THE NEXT STEPS 
-
-//   create a form that takes the value from user input and adds it to the "gifTopic" array
-
-// $(".form-group").on("click", function(event) {
-//     var artist = $("#artInput").val.trim();
-//     topicArray.push(artist);
-
-
-// });
-
-// create a function that takes each topic in the array and remakes the     buttons on the page prepending the user values to the list
-createButtons();
